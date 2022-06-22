a721958e6593177416b401f905f3294e1f7ddbf9
andy <tmgthemultigamer@gmail.com>
2022-06-21 17:38:22 -0700

    changed top bar & added bottom navigation bar

--- app/src/main/java/com/example/metaucapstone/MainActivity.java
+++ app/src/main/java/com/example/metaucapstone/MainActivity.java
@@ -1,14 +1,69 @@
 package com.example.metaucapstone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private ConstraintLayout clMain;
+    private BottomNavigationView bottomNavigationView;
+    BottomNavigationItemView btnSaved;
+    BottomNavigationItemView btnSearch;
+    BottomNavigationItemView btnFriends;
+    Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        clMain = findViewById(R.id.clMain);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        btnSaved = findViewById(R.id.action_saved);
+        btnSearch = findViewById(R.id.action_search);
+        btnFriends = findViewById(R.id.action_friends);
+        fragment = new SearchFragment();
+
+        btnSearch.performClick();
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_baseline_image_24);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_saved:
+                        Log.i(TAG, "saved");
+                        fragment = new SearchFragment();
+                        break;
+                    case R.id.action_search:
+                        Log.i(TAG, "search");
+                        fragment = new SearchFragment();
+                        break;
+                    case R.id.action_friends:
+                        Log.i(TAG, "friends");
+                        fragment = new SearchFragment();
+                        break;
+                    default: break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
     }
 }
