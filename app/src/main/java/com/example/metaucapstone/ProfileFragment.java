b6ab6eda79894281e5b38e6e82a1ac5cadc599aa
andy <tmgthemultigamer@gmail.com>
2022-06-29 17:21:59 -0700

    added profile page

--- app/src/main/java/com/example/metaucapstone/ProfileFragment.java
+++ app/src/main/java/com/example/metaucapstone/ProfileFragment.java
@@ -9,8 +9,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.metaucapstone.models.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileFragment extends Fragment {
+
+    ProgressBar pbProfile;
+    TextView tvDisplayName;
+    TextView tvBio;
+    ImageView ivProfilePic;
 
     public ProfileFragment() { }
 
@@ -29,6 +45,33 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        pbProfile = view.findViewById(R.id.pbProfile);
+        tvDisplayName = view.findViewById(R.id.tvProfileDisplayName);
+        tvBio = view.findViewById(R.id.tvProfileBio);
+        ivProfilePic = view.findViewById(R.id.ivProfile);
 
+        pbProfile.setVisibility(View.VISIBLE);
+        setViews();
+    }
+
+    private void setViews() {
+        FirebaseDatabase.getInstance().getReference()
+                .child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        User user = snapshot.child("Object").getValue(User.class);
+                        tvDisplayName.setText(user.getDisplayName());
+                        tvBio.setText(user.getBio());
+                        Glide.with(ProfileFragment.this)
+                                .load(snapshot.child("ProfilePic").getValue(String.class))
+                                .circleCrop()
+                                .into(ivProfilePic);
+                        pbProfile.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) { }
+                });
     }
 }
