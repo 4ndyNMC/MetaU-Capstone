232e103c373905d830e57de727780935589b804a
andy <tmgthemultigamer@gmail.com>
2022-07-12 14:42:48 -0700

    added search cache for usernames
    when the user clicks on the button to go to where the user search page, the app will make a network request to get a fresh set of usernames, but if that takes too long (greater than 3 seconds), then it will pull from the cached usernames

--- app/src/main/java/com/example/metaucapstone/fragments/FriendsSearchFragment.java
+++ app/src/main/java/com/example/metaucapstone/fragments/FriendsSearchFragment.java
@@ -1,6 +1,8 @@
 package com.example.metaucapstone;
 
+import android.app.Activity;
 import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,23 +17,32 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.example.metaucapstone.MainActivity;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class FriendsSearchFragment extends Fragment {
 
     public static final String TAG = "FriendsSearchFragment";
+    public static final long TIMEOUT_LENGTH = 3000L;
 
     AutoCompleteTextView etSearch;
+    ProgressBar pbFriendsSearch;
+    DatabaseHelper db;
 
     Map<String, String> usernames;
+    boolean[] gotResult = new boolean[1];
 
     public FriendsSearchFragment() { }
     @Override
@@ -50,6 +61,8 @@ public class FriendsSearchFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         etSearch = view.findViewById(R.id.etFriendSearch);
+        pbFriendsSearch = view.findViewById(R.id.pbFriendsSearch);
+        db = new DatabaseHelper(getContext());
 
         etSearch.setEnabled(false);
         etSearch.setOnItemClickListener(dropdownClicked);
@@ -57,34 +70,73 @@ public class FriendsSearchFragment extends Fragment {
         getUsernames();
     }
 
-    AdapterView.OnItemClickListener dropdownClicked = new AdapterView.OnItemClickListener() {
+    private final AdapterView.OnItemClickListener dropdownClicked = new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             String uid = usernames.get(parent.getAdapter().getItem(position));
-            Fragment fragment = new ProfileFragment(uid);
+            Fragment fragment = new com.example.metaucapstone.ProfileFragment(uid);
             getParentFragmentManager().beginTransaction().replace(R.id.flContainer, fragment).commit();
         }
     };
 
-    private void getUsernames() {
-        FirebaseDatabase.getInstance().getReference().child("Usernames")
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        usernames = new HashMap<>();
-                        for (DataSnapshot usernameSnapshot : snapshot.getChildren()) {
-                            usernames.put(usernameSnapshot.getValue(String.class), usernameSnapshot.getKey());
-                        }
-                        String[] names = new String[usernames.size()];
-                        usernames.keySet().toArray(names);
-                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
-                                R.layout.searchtextview, names);
-                        etSearch.setAdapter(adapter);
-                        etSearch.setEnabled(true);
-                    }
+    private final ValueEventListener getUsernames = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot snapshot) {
+            Log.i(TAG, "pulling usernames from network");
+            gotResult[0] = true;
+            usernames = new HashMap<>();
+            for (DataSnapshot usernameSnapshot : snapshot.getChildren()) {
+                usernames.put(usernameSnapshot.getValue(String.class), usernameSnapshot.getKey());
+                db.insertUsername(usernameSnapshot.getKey(), usernameSnapshot.getValue(String.class));
+            }
+            String[] names = new String[usernames.size()];
+            usernames.keySet().toArray(names);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
+                    R.layout.searchtextview, names);
+            etSearch.setAdapter(adapter);
+            etSearch.setEnabled(true);
+            pbFriendsSearch.setVisibility(View.GONE);
+        }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) { }
-                });
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+            gotResult[0] = true;
+        }
+    };
+
+    private void getUsernames() {
+        gotResult[0] = false;
+        pbFriendsSearch.setVisibility(View.VISIBLE);
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Usernames");
+        ref.addListenerForSingleValueEvent(getUsernames);
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                timer.cancel();
+                if (!gotResult[0]) getUsernamesFromDb();
+            }
+        };
+        timer.schedule(timerTask, TIMEOUT_LENGTH);
+    }
+
+    private void getUsernamesFromDb() {
+        Log.i(TAG, "pulling usernames from db");
+        Cursor result = db.getData();
+        usernames = new HashMap<>();
+
+        while (result.moveToNext()) {
+            usernames.put(result.getString(1), result.getString(0));
+            Log.i(TAG, "added " + result.getString(0));
+        }
+        ((MainActivity) getContext()).runOnUiThread(() -> {
+            String[] names = new String[usernames.size()];
+            usernames.keySet().toArray(names);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
+                    R.layout.searchtextview, names);
+            etSearch.setAdapter(adapter);
+            etSearch.setEnabled(true);
+            pbFriendsSearch.setVisibility(View.GONE);
+        });
     }
 }
