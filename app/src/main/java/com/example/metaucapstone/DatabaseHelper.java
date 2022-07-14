85b1fe241245a6a50bf0530431e64701ec528d64
andy <tmgthemultigamer@gmail.com>
2022-07-14 11:02:37 -0700

    added query for friends during splash screen
    the app will query firebase to find the user's friends during the splash screen and store the display name, profile picture url, and serialized version of the user object in the sqlite db

--- app/src/main/java/com/example/metaucapstone/DatabaseHelper.java
+++ app/src/main/java/com/example/metaucapstone/DatabaseHelper.java
@@ -5,21 +5,39 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.example.metaucapstone.models.User;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
+    public static final String TAG = "DatabaseHelper";
+    public static final int DATABASE_VERSION = 1;
+
     public DatabaseHelper(Context context) {
-        super(context, "data.db", null, 1);
+        super(context, "data.db", null, DATABASE_VERSION);
+        initDb(this.getWritableDatabase());
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE usernames (uid TEXT PRIMARY KEY, username TEXT)");
+        Log.i(TAG, "DATABASE CREATED");
+        initDb(db);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS usernames");
+        db.execSQL("DROP TABLE IF EXISTS friends");
+    }
+
+    private void initDb(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS friends (uid TEXT PRIMARY KEY, profilePicUrl TEXT, object BLOB)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS usernames (uid TEXT PRIMARY KEY, username TEXT)");
     }
 
     public boolean hasUsername(String username) {
@@ -39,13 +57,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     public boolean insertUsername(String uid, String username) {
+        if (hasUid(uid)) return updateUsername(uid, username);
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("uid", uid);
         contentValues.put("username", username);
-        if (hasUid(uid)) return updateUsername(uid, username);
+        Log.i(TAG, username + " username");
 
         long result = db.insert("usernames", null, contentValues);
+        db.close();
         return result != -1;
     }
 
@@ -56,8 +76,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         contentValues.put("username", username);
         if (hasUid(uid)) {
             long result = db.update("usernames", contentValues, "uid=?", new String[] {uid});
+            db.close();
             return result != -1;
         }
+        db.close();
         return false;
     }
 
@@ -70,8 +92,66 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return false;
     }
 
-    public Cursor getData() {
+    public Cursor getUsernameData() {
         SQLiteDatabase db = this.getWritableDatabase();
         return db.rawQuery("SELECT * FROM usernames", null);
+    }
+
+    public boolean hasFriend(String uid) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM friends WHERE uid = ?", new String[] {uid});
+        boolean hasValue = cursor.getCount() > 0;
+        cursor.close();
+        return hasValue;
+    }
+
+    // TODO: abstract populating the ContentValues object in insertFriend & updateFriend
+    //       into helper method
+    public boolean insertFriend(String uid, String profilePicUrl, User obj) throws IOException {
+        if (hasFriend(uid)) return updateFriend(uid, profilePicUrl, obj);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream out = null;
+        try {
+            out = new ObjectOutputStream(bos);
+            out.writeObject(obj);
+            out.flush();
+            byte[] serializedUser = bos.toByteArray();
+            SQLiteDatabase db = this.getWritableDatabase();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("uid", uid);
+            contentValues.put("profilePicUrl", profilePicUrl);
+            contentValues.put("object", serializedUser);
+
+            long result = db.insert("friends", null, contentValues);
+            return result != -1;
+        } finally {
+            bos.close();
+        }
+    }
+
+    public boolean updateFriend(String uid, String profilePicUrl, User obj) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream out = null;
+        try {
+            out = new ObjectOutputStream(bos);
+            out.writeObject(obj);
+            out.flush();
+            byte[] serializedUser = bos.toByteArray();
+            SQLiteDatabase db = this.getWritableDatabase();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("uid", uid);
+            contentValues.put("profilePicUrl", profilePicUrl);
+            contentValues.put("object", serializedUser);
+
+            long result = db.update("friends", contentValues, "uid = ?", new String[] {uid});
+            return result != -1;
+        } finally {
+            bos.close();
+        }
+    }
+
+    public Cursor getFriendsData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.rawQuery("SELECT * FROM friends", null);
     }
 }
--- app/src/main/java/com/example/metaucapstone/activities/SplashScreenActivity.java
+++ app/src/main/java/com/example/metaucapstone/activities/SplashScreenActivity.java
@@ -7,31 +7,39 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteConstraintException;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import com.example.metaucapstone.models.User;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.io.IOException;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     public static final String TAG = "SplashScreenActivity";
 
     DatabaseHelper db;
+    DatabaseReference parent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(SplashScreenActivity.this, com.example.metaucapstone.LoginActivity.class));
+        startActivity(new Intent(SplashScreenActivity.this,
+                com.example.metaucapstone.LoginActivity.class));
 
         db = new DatabaseHelper(this);
 
-        FirebaseDatabase.getInstance().getReference().child("Usernames")
-                .addListenerForSingleValueEvent(getNetworkData);
+        parent = FirebaseDatabase.getInstance().getReference();
+//        parent.child("Usernames").addListenerForSingleValueEvent(getUsernames);
+        parent.child("Users").addListenerForSingleValueEvent(getFriends);
         finish();
     }
 
-    private final ValueEventListener getNetworkData = new ValueEventListener() {
+    private final ValueEventListener getUsernames = new ValueEventListener() {
         @Override
         public void onDataChange(@NonNull DataSnapshot snapshot) {
             Log.i(TAG, "retrieved usernames");
@@ -45,6 +53,35 @@ public class SplashScreenActivity extends AppCompatActivity {
                 }
             }
             finish();
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) { }
+    };
+
+    private final ValueEventListener getFriends = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot snapshot) {
+            if (!snapshot.hasChildren()) {
+                return;
+            }
+            for (DataSnapshot friend : snapshot.getChildren()) {
+                parent.child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        User friendObject = snapshot.child(friend.getKey()).child("Object").getValue(User.class);
+                        Log.i(TAG, friendObject.getDisplayName());
+                        try {
+                            db.insertFriend(friend.getKey(), "test.com", friendObject);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) { }
+                });
+            }
         }
 
         @Override
