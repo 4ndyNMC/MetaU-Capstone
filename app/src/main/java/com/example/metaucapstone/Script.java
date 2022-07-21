ee6bb54e6b12c9372f9486e7b5575de705387594
andy <tmgthemultigamer@gmail.com>
2022-07-21 14:40:41 -0700

    added script used to add cuisines to all recipes without cuisines

---
+++ app/src/main/java/com/example/metaucapstone/Script.java
@@ -0,0 +1,75 @@
+package com.example.metaucapstone;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.example.metaucapstone.models.Recipe;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Script {
+
+    public static final String TAG = "Script";
+    public static String RECIPE_INFO_URL = "https://api.spoonacular.com/recipes/";
+    public static String API_KEY = "c6717ec61d2641118f97f5849ecb2c0e";
+    public static String COMPLEX_SEARCH_URL;
+
+    public static void populate() {
+        DatabaseReference recipeReference = FirebaseDatabase.getInstance().getReference().child("Recipes");
+        recipeReference.addListenerForSingleValueEvent(dbAction);
+    }
+
+    private static ValueEventListener dbAction = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot snapshot) {
+            for (DataSnapshot recipeChild : snapshot.getChildren()) {
+                String requestUrl = RECIPE_INFO_URL + recipeChild.getKey() + "/information?includeNutrition=true&apiKey=" + API_KEY;
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(requestUrl)
+                        .build();
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Request request, IOException e) { }
+
+                    @Override
+                    public void onResponse(Response response) throws IOException {
+                        String responseData = response.body().string();
+                        try {
+                            Recipe recipeFromFirebase = recipeChild.child("Object").getValue(Recipe.class);
+                            JSONObject json = new JSONObject(responseData);
+                            Recipe recipeFromSpoonacular = new Recipe(json);
+                            recipeFromSpoonacular.loadData(new JSONObject(responseData));
+                            recipeFromFirebase.setCuisines(new ArrayList<>());
+                            recipeFromFirebase.getCuisines().addAll(recipeFromSpoonacular.getCuisines());
+                            Log.i(TAG, recipeFromFirebase.getName() + " has:");
+                            for (Recipe.Cuisine cuisine : recipeFromFirebase.getCuisines()) {
+                                Log.i(TAG, "              " + cuisine.name());
+                            }
+                            FirebaseDatabase.getInstance().getReference().child("Recipes")
+                                    .child(recipeFromFirebase.getId()).child("Object")
+                                    .setValue(recipeFromFirebase);
+                        } catch (JSONException e) { }
+                    }
+                });
+            }
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) { }
+    };
+}
